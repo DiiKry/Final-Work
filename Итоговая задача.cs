9c58d162db463b9890b23982a9439d6ebf8f825d
Daniil Krylov <dan_42@mail.ru>
2022-05-19 23:28:09 +0300

    Создания программы

--- Итоговая задача.cs
+++ Итоговая задача.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = new string[5] {"hello", "86", "buy", "GitHub", "563"};
+string[] NewArray = new string[array.Length];
+void SecondArray(string[] array, string[] NewArray)
+{
+    int count = 0;
+    for (int index = 0; index < array.Length; index++)
+    {
+    if(array[index].Length <= 3)
+        {
+        NewArray[count] = array[index];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int index = 0; index < array.Length; index++)
+    {
+        Console.Write($"{array[index]} ");
+    }
+    Console.WriteLine();
+}
+SecondArray(array, NewArray);
+PrintArray(NewArray);
